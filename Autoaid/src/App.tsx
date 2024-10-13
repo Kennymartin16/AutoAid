03307daeff84dad7efd5791cfa14484c0512a2c3
--replace-all <kenechukwumartin4@gmail.com>
2024-10-13 19:19:11 +0100

    updating the header and footer

--- Autoaid/src/App.tsx
+++ Autoaid/src/App.tsx
@@ -1,9 +1,20 @@
-// import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/header";
+import Footer from "./Components/footer";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-primary font-bold">Hello World!</h1>
+      <Header />
+      <Routes>
+        <Route path="/blog" element={<Blog/>}/>
+      </Routes>
+      <div className="mt-24">
+      <Home />
+      </div>
+      <Footer/>
     </>
   );
 }
