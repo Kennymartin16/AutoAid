cc6af680ba77db8458a89f7578362ebd5aae7400
Kenechukwu Martin Oguejiofor <145867885+Kennymartin16@users.noreply.github.com>
2024-11-01 03:02:39 +0100

    Update App.tsx

--- Autoaid/src/App.tsx
+++ Autoaid/src/App.tsx
@@ -1,24 +1,56 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog";
-// import Article from "./pages/Article";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Article from "./pages/Article";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import Home from "./pages/Home";
-import Services from "./pages/Service";
+import Services from "./pages/services";
 
 const App: React.FC = () => {
+  const [headerVisible, setHeaderVisible] = useState(true);
+  const footerRef = useRef<HTMLElement | null>(null);
+  
+  useEffect(() => {
+    const footerObserver = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setHeaderVisible(false);
+        } else {
+          setHeaderVisible(true); // Show header when footer is not in view
+        }
+      },
+      { threshold: 0.8 } 
+    );
+
+    const currentFooterRef = footerRef.current;
+
+    if (currentFooterRef) {
+        footerObserver.observe(currentFooterRef);
+     }
+
+     return () => {
+           if (currentFooterRef) {
+            footerObserver.unobserve(currentFooterRef);
+         }
+    };
+  }, []);
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/Services" element={<Services />} />
-      </Routes>
+      <Header isVisible={headerVisible} />
       <div className="mt-24">
-        <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
       </div>
-      <Footer />
+      <footer ref={footerRef}>
+        <Footer/>
+      </footer>
     </>
   );
 };
